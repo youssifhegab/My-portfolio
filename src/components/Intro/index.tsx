0383e288e20a6514858f82f2957e04df138e1760
youssifhegab <youssif.hegab@zyda.com>
2023-01-05 05:05:47 +0200

    Fix ref error

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -10,7 +10,7 @@ import {
 import { IcDownArrow } from "common/icons";
 
 function Intro() {
-  const textRef = useRef();
+  const textRef = useRef(null);
 
   useEffect(() => {
     if (textRef.current)
